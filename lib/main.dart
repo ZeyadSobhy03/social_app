cdedfe390ee7784b5f27ae618600e8e8ff2763a8
ZiyadSobhy <ziad60189@gmail.com>
2026-01-03 15:51:01 +0200

    create social app

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:social_app/presentation/social_app.dart';
+
+void main() {
+  runApp(const SocialApp());
+}
+
+
---
+++ lib/presentation/social_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SocialApp extends StatelessWidget {
+  const SocialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+
+
+    );
+  }
+}
